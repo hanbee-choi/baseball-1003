532c227a402bc2d7274341ec59bfe7b8e9f31643
hanbee.choi <hubaiwoong@proton.me>
2024-06-13 14:01:49 +0900

    [refactoring] 36 - remove duplication

--- test_game.py
+++ test_game.py
@@ -7,17 +7,16 @@ class TestGame(TestCase):
         self.game = Game()
         super().setUp()
 
-    def test_exception_when_input_is_none(self):
-        with self.assertRaises(TypeError):
-            self.game.guess(None)
-
-    def test_exception_when_input_length_is_unmatched(self):
-        guessNumber = "12"
-        self.assert_illegal_argument(guessNumber)
-
     def assert_illegal_argument(self, guessNumber):
         try:
             self.game.guess("%s" % guessNumber)
             self.fail()
         except TypeError:
             pass
+
+    def test_exception_when_input_is_none(self):
+        self.assert_illegal_argument(None)
+
+    def test_exception_when_input_length_is_unmatched(self):
+        self.assert_illegal_argument("12")
+
